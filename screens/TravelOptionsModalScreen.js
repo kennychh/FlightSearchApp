7657647b064bb73ea452d6f2da99aba4f2d85c56
Kenny Chan <hoiyat0210@gmail.com>
2022-10-13 21:38:50 -0400

    child counter

--- screens/TravelOptionsModalScreen.js
+++ screens/TravelOptionsModalScreen.js
@@ -2,18 +2,12 @@ import React, { useState, useEffect, useCallback } from "react";
 import {
   StyleSheet,
   Text,
-  TextInput,
   View,
   useColorScheme,
-  TouchableOpacity,
-  FlatList,
-  ActivityIndicator,
-  SafeAreaView
+  SafeAreaView,
 } from "react-native";
-import { DisplayIcon, IncrementCounter, Button } from "../components";
+import { IncrementCounter, Button } from "../components";
 import {
-  MapsArrowDiagonal,
-  PinAlt,
   NavArrowDown,
   NavArrowUp,
   Check,
@@ -21,14 +15,12 @@ import {
 import DropDownPicker from "react-native-dropdown-picker";
 import { KEY } from "../constants/constants";
 import { themes } from "../constants/theme";
+import { ScrollView } from "react-native-gesture-handler";
 
 export const TravelOptionsModalScreen = ({ route, navigation }) => {
   const colorScheme = useColorScheme();
   const theme = colorScheme === "dark" ? themes.dark : themes.light;
   const styles = style(theme);
-  const defaultResults = [
-    { country: "Current location", iata_code: null, name: "Toronto" },
-  ];
   const [open, setOpen] = useState(false);
   const [counter, setCounter] = useState(1);
   const [value, setValue] = useState("economy");
@@ -38,10 +30,126 @@ export const TravelOptionsModalScreen = ({ route, navigation }) => {
     { label: "Business", value: "business" },
     { label: "First", value: "first" },
   ]);
+  const [ageItems, setAgesItems] = useState([
+    { label: "0", value: 0 },
+    { label: "1", value: 1 },
+    { label: "2", value: 2 },
+    { label: "3", value: 3 },
+    { label: "4", value: 4 },
+    { label: "5", value: 5 },
+    { label: "6", value: 6 },
+    { label: "7", value: 7 },
+    { label: "8", value: 8 },
+    { label: "9", value: 9 },
+    { label: "10", value: 10 },
+    { label: "11", value: 11 },
+    { label: "12", value: 12 },
+    { label: "13", value: 13 },
+    { label: "14", value: 14 },
+    { label: "15", value: 15 },
+  ]);
   const [childrenCounters, setChildrenCounters] = useState(0);
-  const [childrenAges, setChildrenAges] = useState([0, 0, 0, 0, 0, 0, 0, 0]);
+  const [child1Age, setChild1Age] = useState(0);
+  const [child2Age, setChild2Age] = useState(0);
+  const [child3Age, setChild3Age] = useState(0);
+  const [child4Age, setChild4Age] = useState(0);
+  const [child5Age, setChild5Age] = useState(0);
+  const [child6Age, setChild6Age] = useState(0);
+  const [child7Age, setChild7Age] = useState(0);
+  const [child8Age, setChild8Age] = useState(0);
+  const [openChild1, setOpenChild1] = useState(false);
+  const [openChild2, setOpenChild2] = useState(false);
+  const [openChild3, setOpenChild3] = useState(false);
+  const [openChild4, setOpenChild4] = useState(false);
+  const [openChild5, setOpenChild5] = useState(false);
+  const [openChild6, setOpenChild6] = useState(false);
+  const [openChild7, setOpenChild7] = useState(false);
+  const [openChild8, setOpenChild8] = useState(false);
+  const childrenAges = [
+    {
+      id: 0,
+      childAge: child1Age,
+      setChildAge: setChild1Age,
+      open: openChild1,
+      setOpen: setOpenChild1,
+    },
+    {
+      id: 1,
+      childAge: child2Age,
+      setChildAge: setChild2Age,
+      open: openChild2,
+      setOpen: setOpenChild2,
+    },
+    {
+      id: 2,
+      childAge: child3Age,
+      setChildAge: setChild3Age,
+      open: openChild3,
+      setOpen: setOpenChild3,
+    },
+    {
+      id: 3,
+      childAge: child4Age,
+      setChildAge: setChild4Age,
+      open: openChild4,
+      setOpen: setOpenChild4,
+    },
+    {
+      id: 4,
+      childAge: child5Age,
+      setChildAge: setChild5Age,
+      open: openChild5,
+      setOpen: setOpenChild5,
+    },
+    {
+      id: 5,
+      childAge: child6Age,
+      setChildAge: setChild6Age,
+      open: openChild6,
+      setOpen: setOpenChild6,
+    },
+    {
+      id: 6,
+      childAge: child7Age,
+      setChildAge: setChild7Age,
+      open: openChild7,
+      setOpen: setOpenChild7,
+    },
+    {
+      id: 7,
+      childAge: child8Age,
+      setChildAge: setChild8Age,
+      open: openChild8,
+      setOpen: setOpenChild8,
+    },
+  ];
 
-
+  const ChildAgeDropDownPicker = ({ index }) => {
+    return (
+      <View style={[styles.childAgesContainer, { zIndex: 100 - index }]}>
+        <Text style={styles.travellerText}>{`Age of child ${index + 1}`}</Text>
+        <View
+          style={[styles.childDropDownPickerContainer, { zIndex: 100 - index }]}
+        >
+          <DropDownPicker
+            open={childrenAges[index].open}
+            value={childrenAges[index].childAge}
+            items={ageItems}
+            setOpen={childrenAges[index].setOpen}
+            setValue={childrenAges[index].setChildAge}
+            setItems={setAgesItems}
+            style={styles.dropDownPicker}
+            textStyle={styles.dropDownPickerText}
+            dropDownContainerStyle={[styles.dropDownContainer]}
+            theme="DARK"
+            dropDownDirection="BOTTOM"
+            scrollViewProps={{ nestedScrollEnabled: true }}
+            listMode="SCROLLVIEW"
+          />
+        </View>
+      </View>
+    );
+  };
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
@@ -89,7 +197,7 @@ export const TravelOptionsModalScreen = ({ route, navigation }) => {
         <View style={styles.travellerContainer}>
           <View style={styles.travellerInfo}>
             <Text style={styles.travellerText}>Adults</Text>
-            <Text style={styles.ageLimitText}>{'> 15 years'}</Text>
+            <Text style={styles.ageLimitText}>{"> 15 years"}</Text>
           </View>
           <View style={styles.incrementCounterContainer}>
             <IncrementCounter
@@ -99,10 +207,15 @@ export const TravelOptionsModalScreen = ({ route, navigation }) => {
             />
           </View>
         </View>
-        <View style={[styles.travellerContainer, {marginTop: 24, marginBottom: 48}]}>
+        <View
+          style={[
+            styles.travellerContainer,
+            { marginTop: 24, marginBottom: 48 },
+          ]}
+        >
           <View style={styles.travellerInfo}>
             <Text style={styles.travellerText}>Children</Text>
-            <Text style={styles.ageLimitText}>{'< 16 years'}</Text>
+            <Text style={styles.ageLimitText}>{"< 16 years"}</Text>
           </View>
           <View style={styles.incrementCounterContainer}>
             <IncrementCounter
@@ -111,15 +224,22 @@ export const TravelOptionsModalScreen = ({ route, navigation }) => {
             />
           </View>
         </View>
-        <FlatList
-          keyboardDismissMode="on-drag"
-          keyboardShouldPersistTaps="handled"
-        />
-        <Button
-          title={"Done"}
-          styles={styles.buttonStyle}
-          onPress={() => {}}
-        />
+        <ScrollView
+          keyboardShouldPersistTaps="always"
+          nestedScrollEnabled={true}
+          style={{ marginBottom: 24 }}
+        >
+          {childrenCounters >= 1 && <ChildAgeDropDownPicker index={0} />}
+          {childrenCounters >= 2 && <ChildAgeDropDownPicker index={1} />}
+          {childrenCounters >= 3 && <ChildAgeDropDownPicker index={2} />}
+          {childrenCounters >= 4 && <ChildAgeDropDownPicker index={3} />}
+          {childrenCounters >= 5 && <ChildAgeDropDownPicker index={4} />}
+          {childrenCounters >= 6 && <ChildAgeDropDownPicker index={5} />}
+          {childrenCounters >= 7 && <ChildAgeDropDownPicker index={6} />}
+          {childrenCounters >= 8 && <ChildAgeDropDownPicker index={7} />}
+          <View style={{ height: 200 }}></View>
+        </ScrollView>
+        <Button title={"Done"} styles={styles.buttonStyle} onPress={() => {}} />
       </View>
     </SafeAreaView>
   );
@@ -146,6 +266,18 @@ const style = (theme) =>
       marginHorizontal: 24,
       marginBottom: 32,
       zIndex: 100,
+    },
+    childAgesContainer: {
+      alignContent: "center",
+      flexDirection: "row",
+      justifyContent: "space-between",
+      paddingHorizontal: 24,
+      marginBottom: 32,
+      alignItems: "center",
+    },
+    childDropDownPickerContainer: {
+      zIndex: 100,
+      width: 128,
     },
     dropDownContainer: {
       backgroundColor: theme.onBackgroundColor,
@@ -177,16 +309,16 @@ const style = (theme) =>
     },
     incrementCounterContainer: {
       width: 128,
-      alignSelf: 'center'
+      alignSelf: "center",
     },
     travellerContainer: {
-      flexDirection:'row',
-      justifyContent: 'space-between',
+      flexDirection: "row",
+      justifyContent: "space-between",
       height: 48,
       paddingHorizontal: 24,
     },
     travellerInfo: {
-      justifyContent: 'space-between'
+      justifyContent: "space-between",
     },
     travellerText: {
       fontFamily: "Poppins-Medium",
